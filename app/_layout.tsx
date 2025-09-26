ae2b8ce5e4193a2b14ff73302ae78cb22c17fdc5
grantfogle <grantjfogle@gmail.com>
2025-09-25 18:32:45 -0600

    adding initial splash screen

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,9 @@
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { Stack } from 'expo-router';
+import * as SplashScreen from "expo-splash-screen";
 import { StatusBar } from 'expo-status-bar';
+import { useCallback, useEffect, useState } from 'react';
+import { View } from 'react-native';
 import 'react-native-reanimated';
 
 import { useColorScheme } from '@/hooks/use-color-scheme';
@@ -9,15 +12,42 @@ export const unstable_settings = {
   anchor: '(tabs)',
 };
 
+SplashScreen.preventAutoHideAsync();
+
 export default function RootLayout() {
+  const [ready, setReady] = useState(false);
   const colorScheme = useColorScheme();
+
+  //Preload fonts and assets etc.
+  useEffect(() => {
+    (async () => {
+      try {
+        setTimeout(() => {
+          console.log("This runs after 2 seconds!");
+        }, 2000); 
+      } finally {
+        setReady(true);
+      }
+    })();
+  }, []);
+
+  const onLayout = useCallback(() => {
+    if (ready) {
+      SplashScreen.hideAsync();
+    }
+  }, [ready]);
+
+  if (!ready) return null;
+
 
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
-      </Stack>
+      <View style={{flex: 1}} onLayout={onLayout}>
+        <Stack>
+          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+          <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
+        </Stack>
+      </View>
       <StatusBar style="auto" />
     </ThemeProvider>
   );

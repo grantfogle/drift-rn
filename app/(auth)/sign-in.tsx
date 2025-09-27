4d09c88f3d4bfe9df3a7ded103d10973d2aa5f89
grantfogle <grantjfogle@gmail.com>
2025-09-27 13:01:54 -0600

    adding preset styles to themed text component

--- app/(auth)/sign-in.tsx
+++ app/(auth)/sign-in.tsx
@@ -1,11 +1,13 @@
 import { ThemedText } from '@/components/themed-text';
 import { ThemedView } from '@/components/themed-view';
 import { useAuth } from '@/providers/auth';
+import { useTheme } from '@/theme';
 import { router } from 'expo-router';
 import { Button } from 'react-native';
 
 export default function SignInScreen() {
     const { signIn } = useAuth();
+    const {colors} = useTheme();
 
     async function handleSignIn() {
         await signIn();
@@ -17,8 +19,16 @@ export default function SignInScreen() {
     // }
 
     return (
-        <ThemedView style={{ flex: 1, justifyContent: 'center', gap: 12, padding: 24 }}>
-            <ThemedText>Welcome to the sign in screen</ThemedText>
+        <ThemedView style={{ flex: 1, justifyContent: 'center', alignItems: 'center', gap: 12, padding: 24 }}>
+            {/* TODO test to see what is going on with the dark and light mode */}
+            <ThemedText 
+                family="ui"
+                type='title'
+                weight='bold'
+                lightColor={colors.primary} 
+                darkColor={colors.primary}>
+                    Welcome to Drift
+            </ThemedText>
             <Button title="Sign In" onPress={handleSignIn} />
             {/* <Button title="Sign Up" onPress={handleSignUpSelect} /> */}
         </ThemedView>
--- components/themed-text.tsx
+++ components/themed-text.tsx
@@ -1,11 +1,13 @@
-import { StyleSheet, Text, type TextProps } from 'react-native';
-
 import { useThemeColor } from '@/hooks/use-theme-color';
+import { useTheme } from '@/theme';
+import { StyleSheet, Text, type TextProps } from 'react-native';
 
 export type ThemedTextProps = TextProps & {
   lightColor?: string;
   darkColor?: string;
-  type?: 'default' | 'title' | 'defaultSemiBold' | 'subtitle' | 'link';
+  type?: 'default' | 'title' | 'subtitle' | 'body' | 'link' | 'caption';
+  weight?: 'regular' | 'medium' | 'semibold' | 'bold';
+  family?: 'default' | 'brand' | 'ui';
 };
 
 export function ThemedText({
@@ -13,48 +15,50 @@ export function ThemedText({
   lightColor,
   darkColor,
   type = 'default',
+  weight = 'regular',
+  family = 'default',
   ...rest
 }: ThemedTextProps) {
   const color = useThemeColor({ light: lightColor, dark: darkColor }, 'text');
+  const { type: t, fonts, colors } = useTheme();
+
+  const presetByType = {
+    default: t.body,
+    body: t.body,
+    title: t.h1,
+    subtitle: t.h3,
+    caption: t.small,
+    link: t.body
+  } as const;
+
+  const preset = presetByType[type] ?? t.body; // <- fallback
+  // TODO what does using as const do?
+
+  const weightMap = {
+    regular: '400',
+    medium: '500',
+    semibold: '600',
+    bold: '700',
+  } as const;
+
+  const baseStyle = {
+    color: type === 'link' ? colors.primary : color,
+    fontFamily: fonts[family],
+    fontSize: preset.fontSize,
+    lineHeight: preset.lineHeight,
+    // letterSpacing: presetByType[type].letterSpacing ?? 0,
+    fontWeight: weightMap[weight],
+  } as const;
 
   return (
-    <Text
-      style={[
-        { color },
-        type === 'default' ? styles.default : undefined,
-        type === 'title' ? styles.title : undefined,
-        type === 'defaultSemiBold' ? styles.defaultSemiBold : undefined,
-        type === 'subtitle' ? styles.subtitle : undefined,
-        type === 'link' ? styles.link : undefined,
-        style,
-      ]}
+    <Text style={[baseStyle, type === 'link' && styles.linkReset, style]}
       {...rest}
     />
   );
 }
 
 const styles = StyleSheet.create({
-  default: {
-    fontSize: 16,
-    lineHeight: 24,
-  },
-  defaultSemiBold: {
-    fontSize: 16,
-    lineHeight: 24,
-    fontWeight: '600',
-  },
-  title: {
-    fontSize: 32,
-    fontWeight: 'bold',
-    lineHeight: 32,
-  },
-  subtitle: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  link: {
-    lineHeight: 30,
-    fontSize: 16,
-    color: '#0a7ea4',
-  },
+  linkReset: {
+    textDecorationLine: 'underline'
+  }
 });
